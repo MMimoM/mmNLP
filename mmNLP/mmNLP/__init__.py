d635b32c62549c6b1a7a354e9feb6bd93fd0c81f
mehyar mousa <mehyar.mousa@outlook.de>
2022-12-29 20:38:44 +0100

    changed folder structure

--- mmNLP/mmNLP/__init__.py
+++
@@ -1,6 +0,0 @@
-from . import TxT
-from . import TxTArchitectures
-from . import TxTClassification
-from . import WordVectors
-
-__all__ = ['TxTArchitectures', 'TxT', 'TxTClassification', 'WordVectors']
